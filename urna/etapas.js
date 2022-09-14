d442e8a93c57a8b9b583c499d394ea685299636c
Ricardo <ricardodacruz1@hotmail.com>
2022-09-14 02:38:24 -0300

    parte 2

---
+++ urna/etapas.js
@@ -0,0 +1,71 @@
+let etapas = [
+{
+    titulo: 'VEREADOR',
+    numero: 5,
+    candidatos:[
+        {
+            numero: 38111,
+            name: 'Fulano de tal',
+            partido:'ABC',
+            fotos:[
+                {url:'38111.jpg',legenda: 'Vereador'}
+            ]
+        },
+    {
+        numero: 772222,
+        name: 'Beltrano de tal',
+        partido: 'Popular',
+        fotos:[
+            {url:'772222.jpg',legenda: 'Vereador'}
+        ]
+    },   
+
+
+    ]
+},
+
+{
+    titulo: 'PREFEITO',
+    numero: 2,
+    candidatos:[
+        {
+            numero:99,
+            nome: 'Ciclano',
+            partido: 'PT',
+            vice: 'Cic',
+            fotos: [
+                {url: '99.jpg',legenda: 'Prefeito'},
+                {url: '99_2.jpg',legenda: 'Vice-Prefeito',small:true}
+            ]
+        },
+        {
+            numero:84,
+            nome: 'Zulano',
+            partido: 'PMDB',
+            vice: 'Zul',
+            fotos: [
+                {url: '84.jpg',legenda: 'Prefeito'},
+                {url: '84_2.jpg',legenda: 'Vice-Prefeito',small:true}
+            ]
+        },
+        
+
+        ]
+
+
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
